89ff41dead8464a87f8eacf61a2509f4d8272488
Aldo Wanderley Costa Junior <aldowanderley11@gmail.com>
2024-01-06 14:39:34 -0300

    :white_check_mark: adding MovieCard test

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -14,19 +14,21 @@ const MovieCard: React.FC<MovieProps> = ({ title, year, src }) => {
         <Image
           src={src}
           alt={`Movie ${title}`}
-          aria-label={`Movie ${title}`}
           width='100%'
           height={264}
           borderRadius={4}
-          fallback={<Skeleton width={176} height={264} borderRadius={4} />}>
+          fallback={<Skeleton width={176} height={264} borderRadius={4} />}
+          data-testid="movieCard-image"
+          >
         </Image>
-        <Text color='black' fontSize='md' textAlign='start' fontWeight={700} mt="8px">{title}</Text>
+        <Text color='black' fontSize='md' textAlign='start' fontWeight={700} mt="8px" data-testid="movieCard-title">{title}</Text>
         <Text color='#646464'
           fontSize='sm'
           textAlign='start'
           fontWeight={700}
           width='100%'
           mt="4px"
+          data-testid="movieCard-year"
         >{year}</Text>
       </button>
     </GridItem>
