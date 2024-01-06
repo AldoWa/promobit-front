9d7b79825d0b179e661fcf142e38e8782f8c605a
Aldo Wanderley Costa Junior <aldowanderley11@gmail.com>
2024-01-06 11:59:48 -0300

    :sparkles: adding jest and testing library config

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,10 +1,15 @@
 import axios from 'axios';
 
+const {
+  VITE_API_MOVIE_AUTHORIZATION: AUTHORIZATION,
+} = import.meta.env;
+
+
 const api = axios.create({
   baseURL: 'https://api.themoviedb.org/3/',
   headers: {
     accept: 'application/json',
-    Authorization: `Bearer ${import.meta.env.VITE_API_MOVIE_AUTHORIZATION}`,
+    Authorization: `Bearer ${AUTHORIZATION}`,
   },
 });
 
