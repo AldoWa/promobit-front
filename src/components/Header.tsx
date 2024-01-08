5937d4f35cf14b03cabc961f23dabfcc92574e1b
Aldo Wanderley Costa Junior <aldowanderley11@gmail.com>
2024-01-08 16:26:56 -0300

    :sparkles: adding banner movie

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,20 @@
-import { Box, Image } from '@chakra-ui/react'
+import { Box, Image, Container } from '@chakra-ui/react'
 import Logo from '../assets/logo.svg';
-import { Container } from '@chakra-ui/react';
-
+import { Link } from 'react-router-dom';
 
 const Header: React.FC = () => {
   return (
-    <>
-      <Box bg='purple.700' py={4} data-testid="header-box">
-        <Container maxW='1250px' data-testid="header-container">
+    <Box bg='purple.700' py={4} data-testid="header-box">
+      <Container maxW='1250px' data-testid="header-container">
+        <Link to="/">
           <Image src={Logo}
             alt='Logo'
             data-testid="header-logo"
           >
           </Image>
-        </Container>
-      </Box>
-    </>
+        </Link>
+      </Container>
+    </Box>
   );
 };
 
